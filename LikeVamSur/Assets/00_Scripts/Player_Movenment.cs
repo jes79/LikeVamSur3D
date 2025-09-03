dd067172f704e27af63721558335f3c9987fd0ab
jes79 <51398103+jes79@users.noreply.github.com>
2025-09-03 19:45:16 +0900

    PlayerMove
    기본적인 캐릭터의 이동,회전, 애니메이션, 카메라 처리

---
+++ LikeVamSur/Assets/00_Scripts/Player_Movenment.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(CharacterController))] 
+public class Player_Movenment : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+
+    private Vector3 moveDir;
+
+    private CharacterController controller;
+    private Animator animator;
+
+
+    public Vector3 cameraDir = Vector3.zero;
+
+
+    private void Start()
+    {
+
+        controller = GetComponent<CharacterController>();   
+        animator = GetComponent<Animator>();
+        
+
+    }
+
+    private void Update()
+    {
+        Move();
+        Rotate();
+        Animate();
+        CameraMove();
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector3(h,0, v).normalized;
+
+        //Move(), SimpleMove()
+        controller.SimpleMove(moveDir*moveSpeed);
+    }
+
+    void Rotate()
+    {
+        if (moveDir.sqrMagnitude > 0.01)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(moveDir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, 10f*Time.deltaTime);
+        }
+    }
+
+    void Animate()
+    {
+        animator.SetFloat("SPEED", moveDir.magnitude);
+    }
+
+    void CameraMove()
+    {
+        Camera.main.transform.position = Vector3.Lerp(
+            Camera.main.transform.position,
+            transform.position + cameraDir, 
+            2f*Time.deltaTime);
+    }
+}
