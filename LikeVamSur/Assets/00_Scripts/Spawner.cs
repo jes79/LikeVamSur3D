471c84c76aa13152d2dc4b71b36fb595ff68a181
jes79 <51398103+jes79@users.noreply.github.com>
2025-09-03 20:38:42 +0900

    MonsterSpawn
    플레이어를 중심으로 원둘레에서 생성

---
+++ LikeVamSur/Assets/00_Scripts/Spawner.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    //public Vector3 centerPoint = Vector3.zero;
+    public float spawnRadius = 30.0f;
+    public GameObject monsterPrefab;
+    public Transform player;
+    public float spawnInterval = 1.0f;
+
+    public float timer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= spawnInterval)
+        {
+            timer = 0;
+            SpawnMosterAtEdge();
+        }
+    }
+
+    void SpawnMosterAtEdge()
+    {
+        //Vector3 spawnPos = GetRandomPointOnCircleEdge(centerPoint, spawnRadius);
+        Vector3 spawnPos = GetRandomPointOnCircleEdge(player.position, spawnRadius);
+
+        GameObject monster = Instantiate(monsterPrefab, spawnPos, Quaternion.identity);
+    }
+
+    Vector3 GetRandomPointOnCircleEdge(Vector3 center, float radius)
+    {
+        float angle = Random.Range(0.0f, Mathf.PI * 2f);
+        float x = Mathf.Cos(angle)*radius; 
+        float z = Mathf.Sin(angle)*radius;
+
+        return new Vector3(center.x +x , center.y, center.z+ z);
+    }
+}
