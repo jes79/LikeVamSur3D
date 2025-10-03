16a53250bc5044928f45b4828ff0de1a61789ec2
jes79 <51398103+jes79@users.noreply.github.com>
2025-10-03 20:50:05 +0900

    05GetNearestMonster
    가장 가까운 몬스터를 타겟팅

--- LikeVamSur/Assets/00_Scripts/Player_Movenment.cs
+++ LikeVamSur/Assets/00_Scripts/Player_Movenment.cs
@@ -5,8 +5,10 @@ using UnityEngine.EventSystems;
 public class Player_Movenment : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
-
     private Vector3 moveDir;
+
+    public float detectionRadius = 10.0f;
+    public LayerMask monsterLayer;
 
     private CharacterController controller;
     private Animator animator;
@@ -14,6 +16,8 @@ public class Player_Movenment : MonoBehaviour
 
     public Vector3 cameraDir = Vector3.zero;
 
+
+    private Transform Target;
 
     private void Start()
     {
@@ -45,11 +49,49 @@ public class Player_Movenment : MonoBehaviour
 
     void Rotate()
     {
-        if (moveDir.sqrMagnitude > 0.01)
+        Transform nearest = GetNearestMonster();
+
+        if (nearest != null)
         {
-            Quaternion targetRot = Quaternion.LookRotation(moveDir);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, 10f*Time.deltaTime);
+            Vector3 dirToMonster = (nearest.position - transform.position);
+            dirToMonster.y = 0; 
+
+            RotateToQuaternion(dirToMonster);
         }
+
+        else if (moveDir.sqrMagnitude > 0.01)
+        {
+            //중복되어 함수로 빼서 사용 RotateToQuaternion(Vector3 direction)
+            //Quaternion targetRot = Quaternion.LookRotation(moveDir);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, 10f*Time.deltaTime);
+            RotateToQuaternion(moveDir);
+        }
+    }
+
+    void RotateToQuaternion(Vector3 direction)
+    {
+        Quaternion targetRot = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, 10f * Time.deltaTime);
+    }
+
+    Transform GetNearestMonster()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position,detectionRadius, monsterLayer);
+        Transform nearest = null;
+        float minDist = Mathf.Infinity;
+
+        foreach (Collider col in hits)
+        {
+            float dist = Vector3.Distance(transform.position, col.transform.position);
+            if(dist < minDist)
+            {
+                minDist = dist;
+                nearest = col.transform;
+            }
+        }
+
+        return nearest;
+
     }
 
     void Animate()
