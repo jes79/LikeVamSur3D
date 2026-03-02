a6aab88bb54d323088921255c1d806d5c8b3c926
jes79 <51398103+jes79@users.noreply.github.com>
2026-03-02 21:00:31 +0900

    20SkillFrameRepair
    스킬 획득 시 UI 수정

--- LikeVamSur/Assets/00_Scripts/UI/SkillFrame.cs
+++ LikeVamSur/Assets/00_Scripts/UI/SkillFrame.cs
@@ -6,19 +6,19 @@ using UnityEngine.UI;
 public class SkillFrame : MonoBehaviour
 {
     [SerializeField] Image IconImage;
-    //[SerializeField] TextMeshProUGUI LevelText;
-    //[SerializeField] Image SkillFill;
-    //[SerializeField] TextMeshProUGUI TimerText;
+    [SerializeField] TextMeshProUGUI LevelText;
+    [SerializeField] Image SkillFill;
+    [SerializeField] TextMeshProUGUI TimerText;
     float saveCooltime;
 
     public void Initialize(SelectCard selectCard)
     {
         IconImage.sprite = MANAGER.DB.GetSprite(selectCard.db.name);
         
-        //LevelText.text = "Lv." + selectCard.Level.ToString();
+        LevelText.text = "Lv." + selectCard.Level.ToString();
         if(selectCard.db.state == CardState.Active)
         {
-            //SkillFill.gameObject.SetActive(true);
+            SkillFill.gameObject.SetActive(true);
             StopAllCoroutines();
             StartCoroutine(CooltimeCoroutine(selectCard.db, selectCard.Level)); 
         }
@@ -26,20 +26,20 @@ public class SkillFrame : MonoBehaviour
 
     IEnumerator CooltimeCoroutine(CardDB card, int level)
     {
-        /*
+        
         float cooltime = card.baseCooldown - card.cooldownPerLevel * (level - 1);
         float currentCooldown = saveCooltime > 0 ? saveCooltime : cooltime; 
         while(currentCooldown> 0.0f)
         {
             currentCooldown -= Time.deltaTime;
             saveCooltime = currentCooldown;
-            TimerText.text = string.Format("{0:0.0", currentCooldown);
+            TimerText.text = string.Format("{0:0.0}", currentCooldown);
 
             SkillFill.fillAmount = currentCooldown / cooltime;
 
             yield return null;
         }
-        */
-        yield return null;
+
+        StartCoroutine(CooltimeCoroutine(card, level));
     }
 }
